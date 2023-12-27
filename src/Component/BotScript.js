50178eef233946e542ca3a381edb4b0036259c8d
hoahv-2307 <hoang.van.hoa@sun-asterisk.com>
2023-12-27 10:57:04 +0700

    Update: Change bot URL to STG

--- src/Component/BotScript.js
+++ src/Component/BotScript.js
@@ -4,9 +4,9 @@ export default class BotScript extends Component {
   componentDidMount() {
     const botScript = document.createElement("script");
     botScript.id = "efo-script";
-    botScript.src = "http://127.0.0.1:5000/js/faapp.js";
+    botScript.src = "https://sunbotchandev.blob.core.windows.net/stgnode/js/faapp_ls.js";
     botScript.onload = () => {
-      window.efoInit("65702f997bd9e4343b1f23b6", 0, 1);
+      window.efoInit("658a924c78e3fd12873a43f4");
     };
     document.body.appendChild(botScript);
   }
